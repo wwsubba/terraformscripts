d9e3250fb166fbd53596fc6d82b6c3f3e5892aea
wwsubba <118448489+wwsubba@users.noreply.github.com>
2022-11-20 17:50:06 +0530

    Update main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -1,3 +1,18 @@
 terraform {
   experiments = [module_variable_optional_attrs]
 }
+
+
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.0.0"
+    }
+  }
+}
+
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+  features {}
+}
