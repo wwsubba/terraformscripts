a6ca8daa426909ecd5e93e320e3895f2d56b2ec6
wwsubba <118448489+wwsubba@users.noreply.github.com>
2022-11-20 15:01:54 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,21 +6,6 @@ terraform {
     }
   }
 }
-variable "subscription_id" {
-  type = string
-}
-
-variable "client_secret" {
-  type = string
-}
-
-variable "tenant_id" {
-  type = string
-}
-
-variable "client_id" {
-  type = string
-}
 
 # Configure the Microsoft Azure Provider
 provider "azurerm" {
