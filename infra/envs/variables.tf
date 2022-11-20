9f924e52883ced70e7a143b993cfcc91e52f12b2
wwsubba <118448489+wwsubba@users.noreply.github.com>
2022-11-20 21:24:29 +0530

    Delete variables.tf

--- infra/envs/variables.tf
+++
@@ -1,8 +0,0 @@
-variable "environment" {
-  type        = string
-  description = "One of (dev, qa, prod)"
-  validation {
-    condition     = contains(["dev", "qa", "test", "prod"], var.environment)
-    error_message = "Unexpected environment."
-  }
-}
