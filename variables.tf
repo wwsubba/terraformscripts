c84321a8a6225ad1752183ff30468494d367fbe2
wwsubba <118448489+wwsubba@users.noreply.github.com>
2022-11-20 07:25:55 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,12 +1,15 @@
 variable "environment_tag" {
   type        = string
   description = "Environment tag value"
+  default = "subba"
 }
 variable "azure-rg-1" {
   type        = string
   description = "resource group 1"
+  default = "WW-CloudServiceManagement-RG-TBDNov30"
 }
 variable "loc1" {
   description = "The location for this Lab environment"
   type        = string
+  loc1        = "East US"
 }
