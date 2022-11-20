16ac5a99ca6873d6e729f64e8198f5ae61ab4a0d
wwsubba <118448489+wwsubba@users.noreply.github.com>
2022-11-20 15:16:05 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -21,25 +21,30 @@ resource "random_id" "kvname" {
 
 data "azurerm_client_config" "current" {}
 resource "azurerm_key_vault" "kv1" {
-  name                        = random_id.kvname.hex
-  location                    = var.loc1
-  resource_group_name         = var.azure-rg-2
+ name                        = "examplekeyvault"
+  location                    = azurerm_resource_group.example.location
+  resource_group_name         = azurerm_resource_group.example.name
   enabled_for_disk_encryption = true
   tenant_id                   = data.azurerm_client_config.current.tenant_id
   soft_delete_retention_days  = 7
   purge_protection_enabled    = false
+
   sku_name = "standard"
+
   access_policy {
     tenant_id = data.azurerm_client_config.current.tenant_id
     object_id = data.azurerm_client_config.current.object_id
+
     key_permissions = [
-      "get",
+      "Get",
     ]
+
     secret_permissions = [
-      "get", "backup", "delete", "list", "purge", "recover", "restore", "set",
+      "Get",
     ]
+
     storage_permissions = [
-      "get",
+      "Get",
     ]
   }
 }
