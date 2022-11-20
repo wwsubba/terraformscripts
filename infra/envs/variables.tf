98330d1d844e2032ce99b64a56668bf5495d1c34
wwsubba <118448489+wwsubba@users.noreply.github.com>
2022-11-20 17:44:46 +0530

    Create variables.tf

---
+++ infra/envs/variables.tf
@@ -0,0 +1,8 @@
+variable "environment" {
+  type        = string
+  description = "One of (dev, qa, prod)"
+  validation {
+    condition     = contains(["dev", "qa", "test", "prod"], var.environment)
+    error_message = "Unexpected environment."
+  }
+}
