0b0a576255ac978a87cc260c3dd59010a931639a
wwsubba <118448489+wwsubba@users.noreply.github.com>
2022-11-22 12:19:59 +0530

    Update main.tf

--- infra/modules/cloud_keyvault/main.tf
+++ infra/modules/cloud_keyvault/main.tf
@@ -10,15 +10,12 @@ resource "azurerm_key_vault" "kv1" {
   location                    = "East US"
   resource_group_name         = "WW-CloudServiceManagement-RG-TBDNov30"
   enabled_for_disk_encryption = true
-  tenant_id                   = data.azurerm_client_config.current.tenant_id
   soft_delete_retention_days  = 7
   purge_protection_enabled    = false
 
   sku_name = "standard"
 
   access_policy {
-    tenant_id = data.azurerm_client_config.current.tenant_id
-    object_id = data.azurerm_client_config.current.object_id
 
     key_permissions = [
       "Get",
