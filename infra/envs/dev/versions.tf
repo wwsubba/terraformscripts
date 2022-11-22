afa8fc5fafb2cf811d2a27d62ae9c4b45972a420
wwsubba <118448489+wwsubba@users.noreply.github.com>
2022-11-22 15:04:55 +0530

    Update versions.tf

--- infra/envs/dev/versions.tf
+++ infra/envs/dev/versions.tf
@@ -2,14 +2,4 @@ terraform {
   cloud {
     organization = "wwsubba"
   }
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = ">=3.27.0"
-    }
-    github = {
-      source  = "integrations/github"
-      version = "5.5.0"
-    }
-  }
 }
