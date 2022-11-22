090ef58fd5473385cc7d315283f764721ccafabc
wwsubba <118448489+wwsubba@users.noreply.github.com>
2022-11-22 15:04:26 +0530

    Update backend.tf

--- infra/envs/dev/backend.tf
+++ infra/envs/dev/backend.tf
@@ -1,14 +1,5 @@
 terraform {
-   required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = ">=3.27.0"
-    }
-    github = {
-      source  = "integrations/github"
-      version = "5.5.0"
-    }
-  }
+  required_version = "~> 1.2.0"
   cloud {
     organization = "wwsubba"
     workspaces {
