06714ac8f9e8b2836ee4619142ff051a8e8d5524
wwsubba <118448489+wwsubba@users.noreply.github.com>
2022-11-22 14:17:04 +0530

    Create versions.tf

---
+++ infra/envs/dev/versions.tf
@@ -0,0 +1,15 @@
+terraform {
+  cloud {
+    organization = "wwsubba"
+  }
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">=3.27.0"
+    }
+    github = {
+      source  = "integrations/github"
+      version = "5.5.0"
+    }
+  }
+}
