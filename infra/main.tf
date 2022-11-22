00a20d9e5c6853491d282d6eccd608b4c1a7bf2d
wwsubba <118448489+wwsubba@users.noreply.github.com>
2022-11-22 13:43:59 +0530

    Update main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -1,19 +1,6 @@
 terraform {
   experiments = [module_variable_optional_attrs]
 }
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "=3.0.0"
-    }
-  }
-}
-
-# Configure the Microsoft Azure Provider
-provider "azurerm" {
-  features {}
-}
 
 module "api-cloud-keyvault" {
   source = "./modules/cloud_keyvault"
