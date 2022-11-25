d545d807fe27f60244a04e449f39e6c74fda15f4
wwsubba <118448489+wwsubba@users.noreply.github.com>
2022-11-25 15:08:47 +0530

    Update provider.tf

--- infra/envs/dev/provider.tf
+++ infra/envs/dev/provider.tf
@@ -1,1 +1,19 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">=3.0.0"
+    }
+    azuread = {
+      source  = "hashicorp/azuread"
+      version = ">= 1.0"
+    }
+  }
+}
 
+provider "azurerm" {
+  features {}
+}
+
+provider "azuread" {
+}
